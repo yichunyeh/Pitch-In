a8f626b1ab0914f47f8ba31e667d3c7c2bcee2b4
葉怡君 <jennieyeh44@hotmail.com>
2018-03-24 01:24:03 -0400

    Initial Commit

---
+++ NYUHackTests/NYUHackTests.swift
@@ -0,0 +1,36 @@
+//
+//  NYUHackTests.swift
+//  NYUHackTests
+//
+//  Created by YiChunYeh on 3/24/18.
+//  Copyright © 2018 YiChunYeh. All rights reserved.
+//
+
+import XCTest
+@testable import NYUHack
+
+class NYUHackTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
